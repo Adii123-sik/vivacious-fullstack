71234d6783d3c49c092563f64f8ffe20aa29964a
Adii123-sik <asikarwar792@gmail.com>
2026-02-11 11:07:30 +0530

    Fixed duplicate cors import

--- admin/src/config/apiConfig.js
+++ admin/src/config/apiConfig.js
@@ -1,2 +1,1 @@
 export const API_BASE_URL ="https://vivacious-fullstack.onrender.com";
-git add .
--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // CORS (Admin + Frontend + Render safe)
-import cors from "cors";
+
 
 app.use(cors({
   origin: "https://vivacious-fullstack-sn2x.vercel.app",
